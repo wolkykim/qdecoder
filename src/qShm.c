b0e5a72a9fcbb364ee088da70f933adb92586c1a
wolkykim <wolkykim@gmail.com>
2007-06-19 07:29:06 +0000

    git-svn-id: https://svn.qdecoder.org/qdecoder/trunk@110 5a34604f-89d6-db11-b6c0-0015c5f490bc

---
+++ src/qShm.c
@@ -0,0 +1,112 @@
+/************************************************************************
+qDecoder - Web Application Interface for C/C++    http://www.qDecoder.org
+
+Copyright (C) 2001 The qDecoder Project.
+Copyright (C) 1999,2000 Hongik Internet, Inc.
+Copyright (C) 1998 Nobreak Technologies, Inc.
+Copyright (C) 1996,1997 Seung-young Kim.
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+Copyright Disclaimer:
+  Hongik Internet, Inc., hereby disclaims all copyright interest.
+  President, Christopher Roh, 6 April 2000
+
+  Nobreak Technologies, Inc., hereby disclaims all copyright interest.
+  President, Yoon Cho, 6 April 2000
+
+  Seung-young Kim, hereby disclaims all copyright interest.
+  Author, Seung-young Kim, 6 April 2000
+
+Author:
+  Seung-young Kim <wolkykim(at)ziom.co.kr>
+************************************************************************/
+
+#ifdef WITH_IPC
+
+#include <sys/shm.h>
+#include "qDecoder.h"
+#include "qInternal.h"
+
+/**********************************************
+** Usage :
+** Return: In case of success returns shared memory
+**         identifier. Otherwise returns -1;
+** Do    :
+**********************************************/
+int qShmInit(char *pszKeyPath, size_t nSize, int nIfExistsDestroy) {
+	int nShmId;
+
+	// generate unique key using ftok();
+	key_t nShmKey = ftok(pszKeyPath, 'Q');
+	if (nShmKey == -1) return -1;
+
+	// create shared memory
+	if ((nShmId = shmget(nShmKey, nSize, IPC_CREAT | IPC_EXCL | 0666)) == -1) {
+		if(nIfExistsDestroy == 0) return -1;
+
+		// destroy & re-create
+		if(qShmDestroy(pszKeyPath) == 0) return -1;
+		if ((nShmId = shmget(nShmKey, nSize, IPC_CREAT | IPC_EXCL | 0666)) == -1) return -1;
+	}
+
+	return nShmId;
+}
+
+/**********************************************
+** Usage :
+** Return: In case of success returns shared memory
+**         pointer. Otherwise returns -1;
+** Do    :
+**********************************************/
+void *qShmGet(int nShmId) {
+	if (nShmId < 0) return -1;
+	return shmat(nShmId, 0, 0);
+}
+
+/**********************************************
+** Usage :
+** Return: In case of success returns 1.
+**         Otherwise returns 0;
+** Do    :
+**********************************************/
+int qShmFree(int nShmId) {
+	if (nShmId < 0) return 0;
+	if (shmctl(nShmId, IPC_RMID, 0) != 0) return 0;
+	return 1;
+}
+
+/**********************************************
+** Usage :
+** Return: In case of success returns 1,
+**         Otherwise returns 0;
+** Do    :
+**********************************************/
+int qShmDestroy(char *pszKeyPath) {
+	int nShmId;
+
+	// generate unique key using ftok();
+	key_t nShmKey = ftok(pszKeyPath, 'Q');
+	if (nShmKey == -1) return 0;
+
+	// get current shared memory id
+	if ((nShmId = shmget(nShmKey, 0, 0)) == -1) return 0;
+
+	// destory current shared memory
+	return qShmFree(nShmId);
+}
+
+#endif
