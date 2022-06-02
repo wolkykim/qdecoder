dc8a3f2778ca3901b3a0acecf2746e9164611563
Steve Kim <seungyou@twitch.tv>
2022-06-01 21:06:13 -0700

    doc update

--- tests/test_q_urldecode.c
+++ tests/test_q_urldecode.c
@@ -1,7 +1,7 @@
 /******************************************************************************
- * qLibc
+ * qDecoder
  *
- * Copyright (c) 2010-2015 Seungyoung Kim.
+ * Copyright (c) 2000-2022 Seungyoung Kim.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -24,7 +24,7 @@
  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
- *****************************************************************************/
+ ******************************************************************************/
 
 #include "qunit.h"
 #include "qdecoder.h"
